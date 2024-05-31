a6b0065b14fefa66e175cc5c5cb56d79c7e51c12
natevaub <nathan.vaubien@gmail.com>
2024-05-31 18:59:31 +0200

    Initial Commit

---
+++ src/app/components/HeroCarousel.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ImageSlider from "./ImageSlider";
+import "../../styles/image-slider.css";
+
+const CAROUSELS_ASSETS = [
+  "/Stratocaster.jpg",
+  "/Squier.jpg"
+];
+export default function HeroCarousel() {
+  return (
+    <div className='img-slider-container' >
+      <ImageSlider imagesUrls={CAROUSELS_ASSETS} />
+    </div>
+  );
+}
